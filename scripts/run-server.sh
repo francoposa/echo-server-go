0ca38e7fdc6ed5beb2b0d46bd87ec1f07d78f951
francoposa <franco@francoposa.io>
2023-04-18 10:25:18 -0700

    WIP helm chart changes

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o xtrace
 
-echo "***************Starting echo-server service...***************"
+echo "****************Starting echo-server service...****************"
 
 cd /app
 exec ./echo-server server --config config.docker.yaml
